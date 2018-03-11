469e1b17fe165ac88e4dee29c1d242c44fa69618
scull1916 <kpscully@gmail.com>
2018-03-11 16:55:50 -0400

    app is fully functional

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,6 +4,7 @@ var inquirer = require('inquirer');
 //pull in mysql npm and create a connection to the bamazon db
 var mysql = require('mysql');
 
+//declare a variable to hold the details needed to establish the connection to the database
 var connection = mysql.createConnection(
 	{
 		host: "localhost",
@@ -18,19 +19,106 @@ var connection = mysql.createConnection(
 var item = 0;
 var qty = 0;
 var supply = 0;
+var itemPrice = 0;
+var productSel;
 
-connection.connect(function(err)
-	{
-		if (err)
+
+//bulid the point-of-sale function
+function poSale()
+{
+
+		//write connection.query to pull on-hand qty of customer's selected item
+		connection.query("SELECT stock_qty, price, product_name FROM products WHERE item_id="+item, function(err,res)
+	
+		{	
+			
+
+			if (err) throw err;
+
+			{
+				supply = JSON.parse(res[0].stock_qty);
+				itemPrice = JSON.parse(res[0].price);
+				productSel = JSON.stringify(res[0].product_name);
+				
+			}
+
+				//console logs for testing purposes
+				// console.log("product: ", productSel);
+				// console.log("supply: ", supply);
+				// console.log("price: ", itemPrice);
+
+				if (qty > supply)
+				{
+					console.log("I'm sorry, we don't have enough to fill your order.");
+
+				}
+				else
+				{
+					var query = connection.query("UPDATE products SET ? WHERE ?", 
+						[
+						{
+							stock_qty: supply - qty
+						},
+						{
+							item_id: item
+						},
+						]);
+				
+					console.log("Your purchase of " + qty + " " + productSel + " comes to $",qty*itemPrice);
+				}
+
+		});
+
+		setTimeout(function()
 		{
-			console.log("error connecting: " + err.stack);
-		}
+			console.log("Thank you for your purchase!");
+			connection.end();
+		}, 
+		3000);
+		
+}
 
-		console.log("connected as id: " + connection.threadId);
-		queryAllItems();
+//ask the customer what they would like to purchase and how many of the item they wish to purchase
+function shop()
+{
+	inquirer.prompt
+	({
+		name: "item",
+		type: "input",
+		message: "Please enter the stock number of the item you'd like to purchase: "	
+	}).then(function quantity(answer)
+	{
+		item = answer.item;
+		
+
+		inquirer.prompt
+		({
+			name: "quantity",
+			type: "input",
+			message: "Please enter the quantity of this item you'd like to purchase: "
+		}).then(function checkInv(answer)
+		{
+			qty = answer.quantity;
+			
+			console.log("One moment, let me check the inventory.");
+			console.log("**************************************");
+
+			//The clerk is checking the inventory - yes, I know this is supposed to be an online
+			//store. There's a new burgeoning eCommerce customer service movement, 
+			//please see http://bit.ly/Pewcyo for more info. Also, I wanted to practice setTimeout
+
+
+			setTimeout(function()
+			{
+				poSale();
+
+			},
+			1500);
+		});
 	});
+}
 
-
+//Build the "storefront" for customers to select an item for purchase
 function queryAllItems()
 {
 	console.log(
@@ -40,8 +128,10 @@ function queryAllItems()
 	console.log(
 		"INVENTORY");
 
+	//query bamazon to pull the data from the products table
 	connection.query("SELECT * FROM products", function(err, res)
 	{
+
 		for (var i=0; i < res.length; i++)
 		{
 			console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
@@ -54,86 +144,16 @@ function queryAllItems()
 	});
 };
 
-
-function shop()
-{
-	inquirer.prompt
-	({
-		name: "item",
-		type: "input",
-		message: "Please enter the stock number of the item you'd like to purchase: "	
-	}).then(function quantity(answer)
+//connect to the bamazon database
+connection.connect(function(err)
 	{
-		var item = answer.item;
-		console.log(item);
-
-		inquirer.prompt
-		({
-			name: "quantity",
-			type: "input",
-			message: "Please enter the quantity of this item you'd like to purchase: "
-		}).then(function checkInv(answer)
+		if (err)
 		{
-			var qty = answer.quantity;
-			console.log(qty);
+			console.log("error connecting: " + err.stack);
+		}
 
-			console.log("One moment, let me check the backroom.");
-
-			//set timeout for dramatic effect?
-
-
-			//write connection.query to pull on-hand qty of customer's selected item
-			connection.query("SELECT stock_qty FROM products WHERE item_id="+item, function(err,res)
-				{
-					if (err) throw err;
-					supply = parseInt(res[0].stock_qty);
-					console.log("supply: ",supply);
-
-					//(2) compare query result to customer qty request
-					
-					if (qty < res)
-					{
-						function purchase()
-						{
-							var query = connection.query("UPDATE products SET ? WHERE ?", 
-								[
-								{
-									stock_qty: supply - qty
-								},
-								{
-									item_id: item
-								}]);
-						}
-						purchase();
-						console.log(qty);
-						console.log(supply);
-					}
-
-				console.log("Post-purchase inv: ",supply-qty);
-
-				});
-
-
-
-
-			//(2A) if on-hand > than request then run purchase routine; decrement the on-hand inv for selected item
-			//(2B) else, sorry don't have enough to fullfill your order then end? return to shop/choice?
-
-			
-		});
+		console.log("connected as id: " + connection.threadId);
+		queryAllItems();
 	});
 
-			
-// checkInv();
-}
 
-// function checkInv()
-// {
-	
-// }
-
-
-//notes: build a recursive functions to run ops before rolling to the connection.end statement?
-
-//end the connection to the database
-// connection.end();
